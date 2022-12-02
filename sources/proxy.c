c99c855cbe73365903fe50e12b741d9690230c1a
lumenthi <lumenthi@student.42.fr>
2022-12-02 19:30:12 +0100

    :boom: :construction: Getting informations on interfaces

--- sources/proxy.c
+++ sources/proxy.c
@@ -1,6 +1,7 @@
 #include "../headers/malcolm.h"
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <linux/if_arp.h>
 
 static int interface_index(char *name)
 {
@@ -14,7 +15,7 @@ static int interface_index(char *name)
 	}
 
 	ft_bzero(&if_idx, sizeof(struct ifreq));
-	ft_strncpy(if_idx.ifr_name, name, IFNAMSIZ-1);
+	ft_strncpy(if_idx.ifr_name, name, IFNAMSIZ - 1);
 	if (ioctl(sockfd, SIOCGIFINDEX, &if_idx) < 0) {
 		perror("SIOCGIFINDEX");
 		return -1;
@@ -23,33 +24,75 @@ static int interface_index(char *name)
 	return if_idx.ifr_ifindex;
 }
 
+static uint8_t interface_mac(char *name)
+{
+	struct ifreq if_mac;
+
+	ft_bzero(&if_mac, sizeof(struct ifreq));
+	ft_strncpy(if_mac.ifr_name, name, IFNAMSIZ - 1);
+	if (ioctl(sock, SIOCGIFHWADDR, &if_mac) < 0) {
+		perror("SIOCGIFHWADDR");
+		return -1;
+	}
+
+	return 0;
+}
+
+static int arp_request(uint8_t *ip, struct sockaddr_ll sockaddr)
+{
+	socklen_t addr_len = sizeof(struct sockaddr_ll);
+	struct arp_packet packet = {0};
+
+	/* Set the ARP opcode to reply */
+	packet.arp.op = ft_htons(ARP_REPLY);
+
+	/* Changing MAC addresses */
+	ft_memcpy(packet.ethernet.dmac, packet.ethernet.smac, ETH_ADDR_LEN);
+	ft_memcpy(packet.ethernet.smac, g_data.source_mac, ETH_ADDR_LEN);
+	ft_memcpy(packet.arp.tha, packet.arp.sha, ETH_ADDR_LEN);
+	ft_memcpy(packet.arp.sha, g_data.source_mac, ETH_ADDR_LEN);
+
+	/* Swapping IP addresses */
+	ft_memcpy(tmp_ip, packet.arp.sip, IP_ADDR_LEN);
+	ft_memcpy(packet.arp.sip, packet.arp.tip, IP_ADDR_LEN);
+	ft_memcpy(packet.arp.tip, tmp_ip, IP_ADDR_LEN);
+
+	/* debug_packet(&packet.ethernet, &packet.arp); */
+
+	ret = sendto(g_data.sockfd, &packet, sizeof(struct arp_packet), 0,
+		(struct sockaddr *)&src_addr, addr_len);
+
+	return 0;
+}
+
 int ft_proxy(uint8_t *source_ip, uint8_t *target_ip)
 {
-	(void)source_ip;
-	(void)target_ip;
-
-	int len = sizeof(struct ethernet_hdr) + sizeof(struct arp_hdr);
-	char buffer[len];
+	// char buffer[len];
 	int ret;
-	struct sockaddr_ll src_addr;
+	struct sockaddr_ll sockaddr;
 	int if_idx;
-	socklen_t addr_len = sizeof(struct sockaddr_ll);
+
+	ft_bzero(&sockaddr, sizeof(struct sockaddr_ll));
 
 	if ((if_idx = interface_index(g_data.interface)) < 0)
 		return 1;
 
 	printf("%s has index %d\n", g_data.interface, if_idx);
 
-	ret = recvfrom(g_data.sockfd, buffer, len, 0,
-		(struct sockaddr *)&src_addr, &addr_len);
+	/* ret = recvfrom(g_data.sockfd, buffer, len, 0,
+		(struct sockaddr *)&sockaddr, &addr_len); */
 
 	(void)ret;
 
-	printf("PROTO: %d\n", ft_htons(src_addr.sll_protocol));
-	printf("INDEX: %d\n", ft_htons(src_addr.sll_ifindex));
-	printf("HATYPE: %d\n", src_addr.sll_hatype);
-	printf("PKTTYPE: %d\n", src_addr.sll_pkttype);
-	printf("HALEN: %d\n", src_addr.sll_halen);
+	/* Filling sockaddr_ll */
+	sockaddr.sll_family = AF_PACKET;
+	sockaddr.sll_protocol = htons(ETH_P_ALL);
+	sockaddr.sll_ifindex = if_idx;
+	sockaddr.sll_hatype = ARPHRD_ETHER;
+	sockaddr.sll_pkttype = PACKET_HOST;
+
+	arp_request(source_ip, sockaddr);
+	arp_request(target_ip, sockaddr);
 
 	return 0;
 }

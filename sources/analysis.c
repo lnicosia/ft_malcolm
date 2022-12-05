40d0ad32bbf75dffde7c66236732603dd4fa9c6c
lumenthi <lumenthi@student.42.fr>
2022-12-05 20:18:36 +0100

    :construction: Parsing intercepted communication

--- sources/analysis.c
+++ sources/analysis.c
@@ -1,11 +1,20 @@
 #include "../headers/malcolm.h"
+#include <netinet/ip_icmp.h>
+
+static void print_icmp(struct icmphdr *icmp)
+{
+	printf("TYPE: %d\n", icmp->type);
+	printf("CODE: %d\n", icmp->code);
+	printf("CHECKSUM: %x\n", ft_htons(icmp->checksum));
+}
 
 static int sniff_traffic(void *osef)
 {
 	/* TODO: Verbose */
 	(void)osef;
-	int l2fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-
+	int l2fd;
+	struct ethernet_hdr *ethernet;
+	struct iphdr *ip;
 	int len = 65535;
 	char buffer[len];
 	int ret;
@@ -14,6 +23,8 @@ static int sniff_traffic(void *osef)
 
 	ft_bzero(buffer, len);
 
+	l2fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+
 	if (l2fd < 0)
 		fprintf(stderr, "[!] Failed to open layer 2 socket\n");
 
@@ -21,10 +32,24 @@ static int sniff_traffic(void *osef)
 		ret = recvfrom(l2fd, buffer, len, MSG_DONTWAIT,
 			(struct sockaddr *)&src_addr, &addr_len);
 		if (ret > 0) {
-			ft_bzero(buffer, len);
-			printf("\rRECEIVED      \n");
+			if (!filter_out(g_data.source_mac, src_addr.sll_addr, ETH_ADDR_LEN) ||
+				!filter_out(g_data.target_mac, src_addr.sll_addr, ETH_ADDR_LEN)) {
+				ethernet = (struct ethernet_hdr *)buffer;
+				print_mac(src_addr.sll_addr);
+				if (ft_htons(ethernet->type) == ETHERTYPE_IP) {
+					ip = (struct iphdr *)(buffer + sizeof(struct ethernet_hdr));
+					if (ip->protocol == IPPROTO_ICMP)
+						print_icmp((struct icmphdr *)(buffer+(sizeof(struct ethernet_hdr)+sizeof(struct iphdr))));
+				}
+
+				fflush(stdout);
+				printf("\n");
+				ft_bzero(buffer, len);
+			}
 		}
 	}
+
+	(void)print_icmp;
 
 	close(l2fd);
 
